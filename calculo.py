b8f470809da4576bc24934bec0d1a14b2d933d9e
jeremytapv <128632197+jeremytapv@users.noreply.github.com>
2023-04-05 15:04:19 -0400

    Add files via upload

---
+++ calculo.py
@@ -0,0 +1,16 @@
+import math
+diametro = float(input("Ingresa el diámetro: "))
+circunferencia = diametro * math.pi
+radio = diametro / 2
+diamentro = 2 * radio
+print(f"La circunferencia es {circunferencia} y el radio es {radio}")
+diametro = float(input("Ingresa el diámetro: "))
+circunferencia = diametro * math.pi
+radio = diametro / 2
+diamentro = 2 * radio
+print(f"La circunferencia es {circunferencia} y el radio es {radio}")
+diametro = float(input("Ingresa el diámetro: "))
+circunferencia = diametro * math.pi
+radio = diametro / 2
+diamentro = 2 * radio
+print(f"La circunferencia es {circunferencia} y el radio es {radio}")
